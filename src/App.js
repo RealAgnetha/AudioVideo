5bfad2729823fc62511fdc960426b72d43914114
RealAgnetha <carolinatrack@googlemail.com>
2023-01-06 12:22:06 +0100

    new component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Editor from './video_Editor/VideoEditor'
+import Meme_Data from "./video_Editor/Meme_Data";
 
 function App() {
     return (
         <div className="App">
             <Editor/>
+            <Meme_Data/>
         </div>
     );
 }
---
+++ src/video_Editor/Meme_Data.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './css/meme_data.css'
+
+class Meme_Data extends React.Component {
+
+    render = () => {
+        return (
+            <div>
+                <p>Hello World</p>
+            </div>
+        )
+    }
+}
+
+export default Meme_Data
