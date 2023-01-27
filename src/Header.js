87bd3d3a5f7ae94907febb9dd4129b031399d91d
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2023-01-28 00:04:45 +0100

    add dark mode button
    body and smaller h1 change on toggle

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './css/darkMode.css';
+
 const Header = () => {
+
+    const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    };
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme]);
+
+
     return (
-        <header className="header-container">
-            <h1>Meme Editor</h1>
+        <header>
+            <button onClick={toggleTheme} type={theme === 'dark' ? 'dark-btn' : ''}>Toggle Theme</button>
+            <div className="header-container">
+                <h1>Meme Editor</h1>
+            </div>
         </header>
     );
 }
